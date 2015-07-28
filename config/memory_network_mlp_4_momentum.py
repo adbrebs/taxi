3acb6f5ec0e3c0d2c6ceef6eacfea731ef18beb1
Alex Auvolat <alex.auvolat@ens.fr>
2015-07-28 15:49:45 -0400

    Memory net mlp 4

---
+++ config/memory_network_mlp_4_momentum.py
@@ -0,0 +1,57 @@
+from blocks.initialization import IsotropicGaussian, Constant
+from blocks.algorithms import Momentum
+
+from blocks.bricks import Tanh
+
+import data
+from model.memory_network_mlp import Model, Stream
+
+n_begin_end_pts = 5
+
+dim_embeddings = [
+    ('origin_call', data.origin_call_train_size, 10),
+    ('origin_stand', data.stands_size, 10),
+    ('week_of_year', 52, 10),
+    ('day_of_week', 7, 10),
+    ('qhour_of_day', 24 * 4, 10),
+    ('day_type', 3, 10),
+]
+
+embed_weights_init = IsotropicGaussian(0.001)
+
+class MLPConfig(object):
+    __slots__ = ('dim_input', 'dim_hidden', 'dim_output', 'weights_init', 'biases_init', 'embed_weights_init', 'dim_embeddings')
+
+prefix_encoder = MLPConfig()
+prefix_encoder.dim_input = n_begin_end_pts * 2 * 2 + sum(x for (_, _, x) in dim_embeddings)
+prefix_encoder.dim_hidden = [500]
+prefix_encoder.weights_init = IsotropicGaussian(0.01)
+prefix_encoder.biases_init = Constant(0.001)
+prefix_encoder.embed_weights_init = embed_weights_init
+prefix_encoder.dim_embeddings = dim_embeddings
+
+candidate_encoder = MLPConfig()
+candidate_encoder.dim_input = n_begin_end_pts * 2 * 2 + sum(x for (_, _, x) in dim_embeddings)
+candidate_encoder.dim_hidden = [500]
+candidate_encoder.weights_init = IsotropicGaussian(0.01)
+candidate_encoder.biases_init = Constant(0.001)
+candidate_encoder.embed_weights_init = embed_weights_init
+candidate_encoder.dim_embeddings = dim_embeddings
+
+representation_size = 500
+representation_activation = Tanh
+
+normalize_representation = False
+
+step_rule = Momentum(learning_rate=0.1, momentum=0.9)
+
+batch_size = 10000
+# batch_sort_size = 20
+
+# monitor_freq = 2
+
+max_splits = 200
+
+train_candidate_size = 20000
+valid_candidate_size = 20000
+test_candidate_size = 20000
